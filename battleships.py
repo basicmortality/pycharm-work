bc553d0cfac8188bf22c2e2de65b19e7f2ddb742
basicmortality <info@basicmortality.co.uk>
2016-08-20 14:06:13 +0100

    update battleships.py

--- battleships.py
+++ battleships.py
@@ -5,4 +5,20 @@ for i in range(5):
 
 def print_board(board):
     for row in board:
-        print row
+        print " ".join(row)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+guess_row = int(raw_input("Guess Row: "))
+guess_col = int(raw_input("Guess Col: "))
+
+print ship_col
+print ship_row
+
