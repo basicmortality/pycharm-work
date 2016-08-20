d401d65c411d9c86c8c58cf521d3bc4d060af822
basicmortality <info@basicmortality.co.uk>
2016-08-20 13:43:38 +0100

    update battleships.py

--- battleships.py
+++ battleships.py
@@ -5,9 +5,4 @@ for i in range(5):
 
 def print_board(board):
     for row in board:
-        row = 0
-        if row < 6:
-            print board
-            row = row + 1
-        else:
-            return
+        print row
