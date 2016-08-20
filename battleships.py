9766b473100e257333fd9503362931cbcc183fb9
basicmortality <info@basicmortality.co.uk>
2016-08-20 16:36:14 +0100

    tidied up code

--- battleships.py
+++ battleships.py
@@ -5,28 +5,28 @@ board = []
 for x in range(5):
     board.append(["O"] * 5)
 
-
 def print_board(board):
     for row in board:
         print " ".join(row)
 
+print "Welcome to my game of Battleships."
+print " "
+print "Let's play!"
 
-print "Let's play Battleship!"
 print_board(board)
-
 
 def random_row(board):
     return randint(0, len(board) - 1)
 
-
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
-
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
+
+#uncomment to debug computer's guess row/col
+#print ship_row
+#print ship_col
 
 for turn in range(4):
     guess_row = int(raw_input("Guess Row:"))
